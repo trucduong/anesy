69ee981413964267d97cc0031f36ab2fc7dc009f
Truc Duong <dttruc7@gmail.com>
2019-09-09 19:55:24 +0700

    update entities

--- anesy/src/main/java/com/green/entity/Feedback.java
+++ anesy/src/main/java/com/green/entity/Feedback.java
@@ -2,6 +2,8 @@ package com.green.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -11,15 +13,60 @@ import javax.persistence.Table;
 public class Feedback {
 	@Id
 	@GeneratedValue
-	@Column(name="id")
+	@Column(name = "id")
 	private int id;
-	
+
+	@Column(name = "feedback_type")
+	@Enumerated(EnumType.STRING)
 	private FeedbackType feedbackType;
-	
+
+	@Column(name = "author")
 	private int author;
-	
+
+	@Column(name = "target_id")
 	private int targetId;
-	
+
+	@Column(name = "target_type")
 	private FeedbackTargetType targetType;
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public FeedbackType getFeedbackType() {
+		return feedbackType;
+	}
+
+	public void setFeedbackType(FeedbackType feedbackType) {
+		this.feedbackType = feedbackType;
+	}
+
+	public int getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(int author) {
+		this.author = author;
+	}
+
+	public int getTargetId() {
+		return targetId;
+	}
+
+	public void setTargetId(int targetId) {
+		this.targetId = targetId;
+	}
+
+	public FeedbackTargetType getTargetType() {
+		return targetType;
+	}
+
+	public void setTargetType(FeedbackTargetType targetType) {
+		this.targetType = targetType;
+	}
+
 }
