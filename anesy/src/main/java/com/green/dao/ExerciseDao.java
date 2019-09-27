5d6ee2c196f3d9b8b7ca42a7369adf44bee33263
GIT <GIT@ADMIN>
2019-09-27 21:24:48 +0700

    aaa

--- anesy/src/main/java/com/green/dao/ExerciseDao.java
+++ anesy/src/main/java/com/green/dao/ExerciseDao.java
@@ -1,8 +1,13 @@
 package com.green.dao;
 
+import java.util.List;
+
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.green.entity.Exercise;
+import com.green.entity.Lesson;
+import com.green.entity.Subjects;
 
 @Repository
 public class ExerciseDao extends BaseDao<Exercise, Integer> {
@@ -10,5 +15,15 @@ public class ExerciseDao extends BaseDao<Exercise, Integer> {
 	@Override
 	protected Class<Exercise> getEntityClass() {
 		return Exercise.class;
+	}
+	
+	public List<Exercise> findAll(int subId) {
+		StringBuilder hql = new StringBuilder();
+		hql.append("select co from Exercise co where co.subjects.id = :subId");
+		
+		Query query = getFactory().openSession().createQuery(hql.toString(), Subjects.class);
+		query.setParameter("subId", subId);
+		
+		return query.getResultList();
 	}
 }
---
+++ anesy/src/main/java/com/green/model/SubjectModel.java
@@ -0,0 +1,55 @@
+package com.green.model;
+
+import java.util.Date;
+
+public class SubjectModel {
+private int id;
+private String name;
+private String description;
+private String avatar;
+private int author;
+private Date createdAt;
+private String tags;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+public String getAvatar() {
+	return avatar;
+}
+public void setAvatar(String avatar) {
+	this.avatar = avatar;
+}
+public int getAuthor() {
+	return author;
+}
+public void setAuthor(int author) {
+	this.author = author;
+}
+public Date getCreatedAt() {
+	return createdAt;
+}
+public void setCreatedAt(Date createdAt) {
+	this.createdAt = createdAt;
+}
+public String getTags() {
+	return tags;
+}
+public void setTags(String tags) {
+	this.tags = tags;
+}
+}
