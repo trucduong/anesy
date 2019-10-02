1959b2c6f3ed30bb67d6348b2c2947c933df926b
GIT <GIT@ADMIN>
2019-10-02 20:59:30 +0700

    aaaa

--- anesy/src/main/java/com/green/dao/ExerciseDao.java
+++ anesy/src/main/java/com/green/dao/ExerciseDao.java
@@ -2,15 +2,25 @@ package com.green.dao;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import com.green.entity.Exercise;
-import com.green.entity.Lesson;
+
 import com.green.entity.Subjects;
+import com.green.util.ApplicationConfig;
 
 @Repository
 public class ExerciseDao extends BaseDao<Exercise, Integer> {
+	
+	private int pageSize;
+
+	@PostConstruct
+	private void init() {
+		pageSize = Integer.parseInt(ApplicationConfig.getConfig("paging.size"));
+	}
 	
 	@Override
 	protected Class<Exercise> getEntityClass() {
@@ -19,11 +29,37 @@ public class ExerciseDao extends BaseDao<Exercise, Integer> {
 
 	public List<Exercise> findAll(int subId) {
 		StringBuilder hql = new StringBuilder();
-		hql.append("select co from Exercise co where co.subjects.id = :subId");
+		hql.append("select co from Exercise co where co.exercise.id = :subId");
 		
-		Query query = getFactory().openSession().createQuery(hql.toString(), Subjects.class);
+		Query query = getFactory().openSession().createQuery(hql.toString(), Exercise.class);
 		query.setParameter("subId", subId);
 		
 		return query.getResultList();
+	}
+	
+	public List<Exercise> search(String filter, int page) {
+		StringBuilder hql = new StringBuilder();
+		hql.append("from Exercise ca where 1=1");
+		if (filter != null) {
+			hql.append(" and ca.name like '%").append(filter).append("%'");
+		}
+		
+		Query query = getFactory().openSession().createQuery(hql.toString(), Exercise.class);
+		query.setFirstResult((page-1) * pageSize);
+		query.setMaxResults(pageSize);
+		
+		return query.getResultList();
+	}
+
+	public long count(String filter) {
+		StringBuilder hql = new StringBuilder();
+		hql.append("select count(ca) from Exercise ca where 1=1");
+		if (filter != null) {
+			hql.append(" and ca.name like '%").append(filter).append("%'");
+		}
+		
+		Query query = getFactory().openSession().createQuery(hql.toString());
+		Number val = (Number) query.getSingleResult();
+		return val.longValue();
 	}
 }
--- anesy/src/main/java/com/green/model/ExerciseModel.java
+++ anesy/src/main/java/com/green/model/ExerciseModel.java
@@ -1,5 +1,27 @@
 package com.green.model;
 
-public class ExerciseModel {
+import com.green.entity.Subjects;
 
+public class ExerciseModel {
+	private Subjects subjects;
+	private String description;
+	private String name;
+	public Subjects getSubjects() {
+		return subjects;
+	}
+	public void setSubjects(Subjects subjects) {
+		this.subjects = subjects;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
